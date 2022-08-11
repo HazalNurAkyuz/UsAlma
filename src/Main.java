5c91d89b88f9fb9f066d38bc29eb14895dec76e8
Hazal Nur Akyuz <hazalnurakyuz@outlook.com>
2022-08-11 10:26:17 +0300

    a

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
+    public static int us(int taban,int usdeger) {
+        if (usdeger==0)
+            return 1;
+        else if(taban==1)
+            return 1;
+        else
+            return taban*us(taban, usdeger-1);
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(us(3, 4));
     }
 }
